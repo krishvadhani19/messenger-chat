5aae2900ce15b37bce8d77cdf358c4136a71e352
Krish Vadhani <krishvadhani7@gmail.com>
2024-03-31 00:35:02 +0530

    Styling Setup done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import "./globals.css";
+import "./styles/index.scss"
 
 const inter = Inter({ subsets: ["latin"] });
 
